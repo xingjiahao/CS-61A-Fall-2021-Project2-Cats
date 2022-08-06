f2dcf5dfe721baf8d252fd4c6a77ec4f4ef9344d
xjh <xing_jiahao@qq.com>
2022-08-06 19:01:04 +0800

    第一次提交

---
+++ gui_files/precache-manifest.23163c9d047aa831406844fb72b05de3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9510a1fdc07ddd692650e04ebc7808c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/css/2.df9ef6f4.chunk.css"
+  },
+  {
+    "revision": "c328bff35515f271187d",
+    "url": "/static/css/main.dbfcd040.chunk.css"
+  },
+  {
+    "revision": "e663642e071350a7fde4",
+    "url": "/static/js/2.ecdaf578.chunk.js"
+  },
+  {
+    "revision": "c328bff35515f271187d",
+    "url": "/static/js/main.02858080.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
